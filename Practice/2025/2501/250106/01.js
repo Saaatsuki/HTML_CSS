5ae7da1061feca269bb0b3810a990b5c329605a9
Saaatsuki <166793481+Saaatsuki@users.noreply.github.com>
2025-01-06 15:15:15 +0900

    250106

---
+++ Practice/2025/2501/250106/01.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector("button");
+
+btn.addEventListener(`click`,(event) => {
+    const isYes = confirm(`天気は晴れていますか？`);
+    document.querySelector(`.log`).innerHTML
+});;
